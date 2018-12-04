d5d3ddd686c603012f4c40291dcb91f4c274aa25
Carlos Viana <viana.mail@gmail.com>
2018-12-03 23:26:02 -0200

    ajuste styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-import "./reset.css";
-import "./style.css";
+import "./styles/reset.css";
+import "./styles/style.css";
 
 render(<App />, document.getElementById("app"));
